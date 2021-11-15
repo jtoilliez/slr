d57f9c57b595a00d37103ab69949b0b091ef9b09
Jean T <46502166+jtoilliez@users.noreply.github.com>
2021-11-14 16:59:40 -0800

    Update base.py

--- slr/base.py
+++ slr/base.py
@@ -2,21 +2,41 @@ import numpy as np
 M_TO_FT = 3.281
 
 
-
 class Dataset:
-    def __init__(self,
+    def __init__(
+        self,
         description: str,
         short_name: str,
         units: str,
         probability: float,
         data: dict = None
     ) -> None:
+        """Dataset class replicates structure from data.json file
+
+        Parameters
+        ----------
+        description : str
+            A description of the dataset that includes the origin, the year,
+            the assumptions, etc. of the dataset
+        short_name : str
+            A short name used to label plots, etc.
+        units : str
+            Standard set of units which can only be one of the following:
+            ft, m, cm, in
+        probability : float
+            The value of the Cumulative Distribution Function associated with the data;
+            for example, .9
+        data : dict, optional
+            The actual data which contains the SLR projects, given as a dictionary,
+            with keys 'x' (years) and 'y' (SLR values), by default None
+
+        """
 
         # Dataset has certain attributes
         self.units = units
         self.description = description
 
-        if probability >=0. and probability <= 1.:
+        if probability >= 0. and probability <= 1.:
             self.probability = probability
         else:
             raise ValueError(
@@ -25,9 +45,10 @@ class Dataset:
         # Save the data as Data
         self.data = Data(units=units, data=data)
 
+
 class Data:
     def __init__(self, units: str, data: dict) -> None:
-        
+
         # Check for length of data
         if len(data["x"]) != len(data["y"]):
             raise ValueError(
@@ -43,12 +64,28 @@ class Data:
         self.units = units
 
     def _check_units(self, units: str) -> None:
+        """Check units and makes sure it's within the standard set
+
+        Parameters
+        ----------
+        units : str
+            Can only be one of ft, in, m, and cm
+
+        """
         if not (units in ['ft', 'in', 'm', 'cm']):
             raise ValueError(
-                f"Units {units} are not supported; only use 'ft', " 
+                f"Units {units} are not supported; only use 'ft', "
                 f"'in', 'm', and 'cm'.")
 
     def _convert(self, to_units: str) -> None:
+        """Convert values from units in data to values specified by to_units; the
+        units can only be one of ft, in, m, and cm. The conversion is in place.
+
+        Parameters
+        ----------
+        to_units : str
+            One of ft, in, m, or cm
+        """
         # Check the units
         self._check_units(to_units)
 
@@ -58,38 +95,38 @@ class Data:
                 fac = 12.
             if to_units == 'ft':
                 fac = 1.
-            if to_units == 'm' :
+            if to_units == 'm':
                 fac = 1 / M_TO_FT
-            if to_units == 'cm' :
+            if to_units == 'cm':
                 fac = 1 / M_TO_FT * 100
         if self.units == 'in':
             if to_units == 'in':
                 fac = 1.
             if to_units == 'ft':
                 fac = 1/12.
-            if to_units == 'm' :
+            if to_units == 'm':
                 fac = 1/12. * 1 / M_TO_FT
-            if to_units == 'cm' :
+            if to_units == 'cm':
                 fac = 1/12. * 1 / M_TO_FT * 100
         if self.units == 'm':
             if to_units == 'in':
                 fac = 12. * M_TO_FT
             if to_units == 'ft':
                 fac = M_TO_FT
-            if to_units == 'm' :
+            if to_units == 'm':
                 fac = 1.
-            if to_units == 'cm' :
+            if to_units == 'cm':
                 fac = 100.
         if self.units == 'cm':
             if to_units == 'in':
                 fac = 12. * M_TO_FT * 1/100.
             if to_units == 'ft':
                 fac = M_TO_FT * 1/100
-            if to_units == 'm' :
+            if to_units == 'm':
                 fac = 1/100.
-            if to_units == 'cm' :
+            if to_units == 'cm':
                 fac = 1.
-        
+
         # Apply the transformation
         self.y = self.y * fac
         self.units = to_units
