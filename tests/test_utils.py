5c7ceca120d9a7b2c7fe21955ea4c1d776ae5542
Jean T <46502166+jtoilliez@users.noreply.github.com>
2021-11-28 15:36:50 -0800

    refactoring and new tests

---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+from utils import ALL_SCENARIOS
+from data import Data
+
+
+def test_health_json_data():
+    # Test health of the master SLR dataset
+    for _, pack in ALL_SCENARIOS.items():
+        assert "description" in pack
+        assert "station ID (CO-OPS)" in pack
+        assert "scenarios" in pack
+        for elem in pack["scenarios"]:
+            assert "short name" in elem
+            assert "units" in elem
+            assert "probability (CDF)" in elem
+            # Make sure probability checks out
+            assert (
+                isinstance(elem["probability (CDF)"], float)
+                or elem["probability (CDF)"] is None
+            )
+            assert "baseline year" in elem
+            assert "data" in elem
+            # Now look at the data itself
+            assert "x" in elem["data"]
+            assert "y" in elem["data"]
+            assert isinstance(elem["data"]["x"], list)
+            assert isinstance(elem["data"]["y"], list)
+
